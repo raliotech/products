8f9434274ed4f3dc8d6afcf09b6e805236f06a3a
Gaurang Gupta <iotgupta@gmail.com>
2025-06-02 14:22:55 +0530

    Add Mercury v3

--- core/board_manager/esp8266/cores/esp8266/core_version.h
+++ core/board_manager/esp8266/cores/esp8266/core_version.h
@@ -1,10 +1,24 @@
-#define ARDUINO_ESP8266_GIT_VER   0x210897ef
-#define ARDUINO_ESP8266_GIT_DESC  3.1.2
-#define ARDUINO_ESP8266_VERSION   3.1.2
+#ifndef ARDUINO_ESP8266_GIT_VER
+#define ARDUINO_ESP8266_GIT_VER 0x00000000
+#endif
 
-#define ARDUINO_ESP8266_MAJOR     3
-#define ARDUINO_ESP8266_MINOR     1
-#define ARDUINO_ESP8266_REVISION  2
+#ifndef ARDUINO_ESP8266_GIT_DESC
+#define ARDUINO_ESP8266_GIT_DESC unspecified
+#endif
 
-#define ARDUINO_ESP8266_RELEASE   "3.1.2"
-#define ARDUINO_ESP8266_RELEASE_3_1_2
+#ifndef ARDUINO_ESP8266_MAJOR
+#define ARDUINO_ESP8266_MAJOR     0
+#endif
+
+#ifndef ARDUINO_ESP8266_MINOR
+#define ARDUINO_ESP8266_MINOR     0
+#endif
+
+#ifndef ARDUINO_ESP8266_REVISION
+#define ARDUINO_ESP8266_REVISION  0
+#endif
+
+// ARDUINO_ESP8266_RELEASE is defined for released versions as a string containing the version name, i.e. "2_3_0_RC1"
+// ARDUINO_ESP8266_RELEASE is used in the core internally. Please use ESP.getCoreVersion() function instead.
+
+// ARDUINO_ESP8266_RELEASE_<version number> are defined for releases, for use in #ifdef... constructs
